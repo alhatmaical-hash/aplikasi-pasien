f444b7db67a382eea4d91b14ec7f69cb0d53a793
alhatma <alhatmaical@gmail.com>
2026-04-03 10:24:02 +0900

    Update rekap_medis.py

--- rekap_medis.py
+++ rekap_medis.py
@@ -587,8 +587,8 @@ elif menu == "Analisis Istirahat":
             df_view['Istirahat Jam'] = df_final['istirahat_jam'].replace(0, '-')
             
             st.dataframe(df_view, hide_index=True, use_container_width=True)
-    else:
-        st.warning("⚠️ Tidak ada data untuk kategori filter ini.")
+       else:
+            st.warning("⚠️ Tidak ada data untuk kategori filter ini.")
     else:
         st.info("ℹ️ Database kosong pada periode ini.")
 
