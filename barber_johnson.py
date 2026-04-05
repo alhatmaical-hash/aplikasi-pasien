c136c18ed789129afeaf4c10fdc8a386b7086004
alhatma <alhatmaical@gmail.com>
2026-04-05 13:56:22 +0900

    Update barber_johnson.py

--- barber_johnson.py
+++ barber_johnson.py
@@ -34,7 +34,7 @@ if 'logged_in' not in st.session_state:
     st.session_state['logged_in'] = False
 
 if not st.session_state['logged_in']:
-    st.title("🔐 Login Klinik Apps")
+    st.title("🔐 Login Barber Johnson Apps")
     user = st.text_input("Username")
     pw = st.text_input("Password", type="password")
     if st.button("Login"):
